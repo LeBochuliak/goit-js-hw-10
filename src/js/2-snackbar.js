9edc29034382e1fe90919f12ff644c04a801a9d8
Lena <lena.bochuliak@gmail.com>
2025-03-09 19:50:35 +0100

    Fix bags

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,6 +5,7 @@ import iconError from "../img/error.png";
 import iconCaution from "../img/caution.png";
 import iconOk from "../img/ok.png";
 
+const form = document.querySelector(".form");
 const formButton = document.querySelector(".form-button");
 const state = document.querySelectorAll('input[type="radio"]');
 const fieldset = document.querySelector(".fieldset-state");
@@ -19,16 +20,24 @@ inputDelay.addEventListener("input", (e) => {
     }
 });
 
-state.forEach(input => {
-    input.addEventListener("change", () => {
-        if (input.value === "fulfilled") {            
-            stateChoice = true;
-            fieldset.classList.add("active");
-        } else if (input.value === "rejected") {
-            stateChoice = false;
-            fieldset.classList.add("active");
-        } else {
-            iziToast.show({
+formButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let isAnySelected = false;
+    state.forEach(input => {
+        if (input.checked) {
+            isAnySelected = true;
+            if (input.value === "fulfilled") {            
+                stateChoice = true;
+                fieldset.classList.add("active");
+            } else if (input.value === "rejected") {
+                stateChoice = false;
+                fieldset.classList.add("active");
+            }
+        }
+    });
+
+    if (!isAnySelected) {
+        iziToast.show({
             title: "Caution",
             titleColor: "#fff",
             titleSize: "16px",
@@ -38,12 +47,13 @@ state.forEach(input => {
             backgroundColor: "#ffa000",
             iconUrl: iconCaution,
             position: "topRight"
-            });
-        }
-    });
-});
+        });
+        return;
+    }
 
-formButton.addEventListener("click", (e) => {
+    form.reset(); 
+    fieldset.classList.remove("active");
+
     const currentDelay = delay;
     const currentStateChoice = stateChoice;
     const promise = new Promise((resolve, reject) => {
@@ -52,42 +62,39 @@ formButton.addEventListener("click", (e) => {
                 resolve(`Fulfilled promise in ${currentDelay}ms`);
             } else {
                 reject(`Rejected promise in ${currentDelay}ms`);
-            };
+            }
         }, currentDelay);
     });
 
-
     promise
-            .then(value => {
-                iziToast.show({
-                    title: "OK",
-                    titleColor: "#fff",
-                    titleSize: "16px",
-                    messageColor: "#fff",
-                    messageSize: "16px",
-                    message: value,
-                    backgroundColor: "#59a10d",
-                    iconUrl: iconOk,
-                    position: "topRight"
-                });
-            })
-            .catch(error => {
-                iziToast.show({
-                    title: "Error",
-                    titleColor: "#fff",
-                    titleSize: "16px",
-                    messageColor: "#fff",
-                    messageSize: "16px",
-                    message: error,
-                    backgroundColor: "#ef4040",
-                    iconUrl: iconError,
-                    position: "topRight"
-                });
+        .then(value => {
+            iziToast.show({
+                title: "OK",
+                titleColor: "#fff",
+                titleSize: "16px",
+                messageColor: "#fff",
+                messageSize: "16px",
+                message: value,
+                backgroundColor: "#59a10d",
+                iconUrl: iconOk,
+                position: "topRight"
             });
-
-        
-e.preventDefault();
-})
+        })
+        .catch(error => {
+            iziToast.show({
+                title: "Error",
+                titleColor: "#fff",
+                titleSize: "16px",
+                messageColor: "#fff",
+                messageSize: "16px",
+                message: error,
+                backgroundColor: "#ef4040",
+                iconUrl: iconError,
+                position: "topRight"
+            });
+        });
+    
+});
 
 
   
